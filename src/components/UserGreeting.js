f465c18a340c7f0b8f797440724329f259a283a0
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 17:08:55 +0530

    conditional rendering using ternary operator_true

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react'
 export class UserGreeting extends Component {
     constructor(props) {
         super(props)
-
         this.state = {
             IsLoggedIn: true
         }
     }
 
     render() {
-        let message
+        return(
+            this.state.IsLoggedIn ?
+            <div>Welcome Shagufta</div> :
+            <div>Welcome Guest</div>
+        )
+        /*let message 
         if (this.state.IsLoggedIn) {
             message = <div>Welcome Shagufta</div>
         }
         else {
             message = <div>Welcome Guest</div>
         }
-        return <div>{message}</div>
+        return <div>{message}</div>*/
         /*    return <div>Welcome Shagufta</div>
         }
         else {
