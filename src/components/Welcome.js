a579ce6bdc1cc8d907d9e15fdd09357fc1ae2295
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 16:38:13 +0530

    destructing staeful components //

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,9 +3,8 @@ export class Welcome extends Component {
 
     render() {
         const { name, alias } = this.props
-        const { state1, state2 } = this.state
+        //const { state1, state2 } = this.state
         return <h1>welcome ms {name} your alias is {alias}</h1>
     }
 }
-
 export default Welcome
