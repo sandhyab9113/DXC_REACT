ea985a8dec3059376973d9389f3777dc620f58f4
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 17:14:47 +0530

    name lists

--- src/App.js
+++ src/App.js
@@ -6,19 +6,36 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import ClassClick from './components/ClassClick';
 import FunctionClick from './components/FunctionClick';
+
 import EventBind from './components/EventBind';
+
 import ParentComponent from './components/ParentComponent';
+
 import UserGreeting from './components/UserGreeting';
+
+import NameList from './components/NameList';
+
+
+
+
+
 
 
 function App() {
+
   return (
+
     <div className="App">
 
+      <NameList />
+
       <UserGreeting />
+
       <ParentComponent />
+
       <EventBind />
     </div>
   );
 }
+
 export default App;
