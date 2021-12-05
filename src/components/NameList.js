860955700c8d5bd65c6d4bc06481992c8c83258b
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 17:16:23 +0530

    map using a const nameslist

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,14 +1,12 @@
 import React from 'react'
-
 function NameList() {
     const names = ['Shaggu', 'Shayi', 'Sony']
     return (
         <div>
-            <h2>{names[0]}</h2>
-            <h2>{names[1]}</h2>
-            <h2>{names[2]}</h2>
+
+            {names.map(name => <h2>{name}</h2>)}
+
         </div>
     )
 }
-
 export default NameList
