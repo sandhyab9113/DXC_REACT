5236569166fe8d828244dac4ea18f0d5004a9273
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 18:06:00 +0530

    topics selection added but didnt assigned handler

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,24 +2,47 @@ import React, { Component } from 'react'
 export class Form extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             username: '',
-             comments: ''
+            username: '',
+            comments: '',
+            topic: 'React'//added another property
         }
     }
-    /*handleUsernameChange = (event) => {
+    handleUsernameChange = (event) => {
         this.setState({
             username: event.target.value
         })
-    }*/
-//for app to react to the text we added a handler
+    }
+
+    handleCommentsChange = (event) => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
+    //for app to react to the text we added a handler for username &
+    //added handler to the comment box too
+    //added topics div but wont be able to select the options as no handler is added
     render() {
         return (
             <form>
                 <div>
                     <label>username</label>
-                    <input type='text' />
+                    <input type='text' value={this.state.username} onChange={this.handleUsernameChange} />
+                </div>
+                <div>
+                    <label>comments</label>
+                    <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+                </div>
+                <div>
+                    <label>topics</label>
+                    <select value={this.state.topic}>
+                        <option value="react" >React</option>
+                        <option value="angular">Angular</option>
+                        <option value="vue">Vue</option>
+
+                    </select>
+
                 </div>
 
             </form>
