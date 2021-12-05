4ec7edcd053e87d76ad04a4a5914553a5f8c7b9c
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 16:58:54 +0530

    pass a ref of a method as props to the child component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
-
 import Counter from './components/Counter';
 
 import ClassClick from './components/ClassClick';
@@ -12,16 +11,27 @@ import FunctionClick from './components/FunctionClick';
 
 import EventBind from './components/EventBind';
 
+import ParentComponent from './components/ParentComponent';
+
+
+
+
+
+
+
 function App() {
 
   return (
 
     <div className="App">
 
+      <ParentComponent />
+
       <EventBind />
+
     </div>
 
   );
-
 }
+
 export default App;
