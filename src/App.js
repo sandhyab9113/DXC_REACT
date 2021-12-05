d8e7cc2a00806bfde4a8d03665e46263a3d693ae
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 17:57:41 +0530

    form handling

--- src/App.js
+++ src/App.js
@@ -6,26 +6,36 @@ import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import Table from './components/Table';
 import StyleSheet from './components/StyleSheet';
+
 import Inline from './components/Inline';
+
 import './appStyles.css';
+
 import styles from './appStyles.module.css';
+
+import Form from './components/Form';
+
+
+
+
+
 
 
 function App() {
+
   return (
 
     <div className="App">
+
+      <Form />
 
       <h1 className={styles.success}>success</h1>
 
       <h1 className='error'>error</h1>
 
       <StyleSheet enabled={true} />
-
       <Inline />
-
       <Table />
-
       <NameList />
       <UserGreeting />
       <ParentComponent />
