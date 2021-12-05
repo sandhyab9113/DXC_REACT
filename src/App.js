73fc402019f3ae4a41fe5a7899b44b48f6914a08
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 18:21:36 +0530

    fragments in table stage 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
@@ -7,37 +6,22 @@ import NameList from './components/NameList';
 import Table from './components/Table';
 import StyleSheet from './components/StyleSheet';
 import Inline from './components/Inline';
-
 import './appStyles.css';
-
 import styles from './appStyles.module.css';
-
 import Form from './components/Form';
-
 import { Fragment } from 'react';
-
 import Fragments from './components/Fragments';
-
-
-
-
-
+import NewTable from './components/NewTable';
 
 
 function App() {
-
   return (
-
     <div className="App">
-
+      <NewTable />
       <Form />
-
       <Fragments />
-
       <h1 className={styles.success}>success</h1>
-
       <h1 className='error'>error</h1>
-
       <StyleSheet enabled={true} />
       <Inline />
       <Table />
@@ -48,5 +32,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
