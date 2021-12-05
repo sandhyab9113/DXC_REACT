dfd299ad938032e9e37aba9585e59f062105ef90
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 17:54:14 +0530

    Modules are locally stored

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import './App.css';
 
-
-
-
-
-
-
-
-
 import EventBind from './components/EventBind';
-
 import ParentComponent from './components/ParentComponent';
-
 import UserGreeting from './components/UserGreeting';
-
 import NameList from './components/NameList';
-
 import Table from './components/Table';
-
 import StyleSheet from './components/StyleSheet';
-
 import Inline from './components/Inline';
-
 import './appStyles.css';
-
 import styles from './appStyles.module.css';
 
 
-
-
-
-
-
 function App() {
-
   return (
 
     <div className="App">
@@ -42,7 +20,7 @@ function App() {
 
       <h1 className='error'>error</h1>
 
-      <StyleSheet enabled={false} />
+      <StyleSheet enabled={true} />
 
       <Inline />
 
--- src/components/Inline.js
+++ src/components/Inline.js
@@ -1,17 +1,17 @@
 import React from 'react'
-
 const heading = {
     fontSize: '74px',
     color: 'blue',
     textDecoration: 'underline'
 }
-
 function Inline() {
     return (
         <div>
-            <h1 style={heading}>Uderlined Headding</h1>
+
+            <h1 className='error'> Inline Error</h1>{/*error style is locally scoped no need to import */}
+            <h1 style={heading}>inline Heading</h1>
+
         </div>
     )
 }
-
 export default Inline
