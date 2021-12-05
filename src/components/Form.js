a70b24e7ec9028d1bf875b019cd1568b2a6ac807
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 18:00:14 +0530

    prior to the addition of handler

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react'
-
 export class Form extends Component {
     constructor(props) {
         super(props)
-
+    
         this.state = {
              username: '',
              comments: ''
         }
     }
-    handleUsernameChange = (event) => {
+    /*handleUsernameChange = (event) => {
         this.setState({
             username: event.target.value
         })
-//this just creates just an empty text box with a lable- username
-    }
+    }*/
+//for app to react to the text we added a handler
     render() {
         return (
             <form>
                 <div>
                     <label>username</label>
-                    <input type='text' value={this.state.username} onChange = {this.handleUsernameChange} />
+                    <input type='text' />
                 </div>
 
             </form>
         )
     }
 }
-
 export default Form
