0818358e1e44924d0cfc70fb4b6d29316d60ec95
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 17:03:09 +0530

    conditional rendering -- if/else

--- src/App.js
+++ src/App.js
@@ -4,34 +4,21 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
-
 import ClassClick from './components/ClassClick';
-
 import FunctionClick from './components/FunctionClick';
-
 import EventBind from './components/EventBind';
-
 import ParentComponent from './components/ParentComponent';
-
-
-
-
-
+import UserGreeting from './components/UserGreeting';
 
 
 function App() {
-
   return (
-
     <div className="App">
 
+      <UserGreeting />
       <ParentComponent />
-
       <EventBind />
-
     </div>
-
   );
 }
-
 export default App;
