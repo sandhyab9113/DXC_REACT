8cb0459a5d983cce77238b33fb83e1287dedeabf
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 13:46:32 +0530

    handling state change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="sandy" college="IIT">
+      <Message />
+      <Greet name="Sandy" college="IIT">
         <p> this is children property</p>
       </Greet>
-      <Greet name="sandhya" college="Oxford" />
-      <Greet name="tanveer" />
-
-      <Welcome />
-
+      <Greet name="Sandy" college="Oxford">
+        <button>click me</button>
+      </Greet>
       <Greet />
-      <Greet />    </div>
+      <Welcome name="San" alias="San" />
+    </div>
   );
 }
-
 export default App;
