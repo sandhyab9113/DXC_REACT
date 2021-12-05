417a292081b873d5c7763d6c7763d2d1fc0419c7
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 17:05:58 +0530

    conditional rendering using variableMessage _false

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react'
 export class UserGreeting extends Component {
     constructor(props) {
         super(props)
-
         this.state = {
             IsLoggedIn: false
         }
     }
 
     render() {
+        let message
         if (this.state.IsLoggedIn) {
-            return <div>Welcome Shagufta</div>
+            message = <div>Welcome Shagufta</div>
+        }
+        else {
+            message = <div>Welcome Guest</div>
+        }
+        return <div>{message}</div>
+        /*    return <div>Welcome Shagufta</div>
         }
         else {
             return <div>Welcome Guest</div>
-        }
+        }*/
         /*return (
             <div>
                <div>Wlcome Shagufta</div>
