02a09b2af21d5e6fec1054b33e16e3993d315824
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 16:40:21 +0530

    event handling

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import ClassClick from './components/ClassClick';
+import FunctionClick from './components/FunctionClick';
 
 function App() {
-  return (
-    <div className="App">
-      <Message />
-      <Counter />
-      <Greet name="San" college="IIT">
 
+  return (
+
+    <div className="App">
+
+      <Message />
+
+
+      <ClassClick />
+
+      <FunctionClick />
+
+      <Counter />
+
+      <Greet name="Shaggu" college="IIT">
         <p> this is children property</p>
       </Greet>
-      <Greet name="San" college="Oxford" >
-
+      <Greet name="Shayi" college="Oxford" >
         <button>click me</button>
-
       </Greet >
-
       <Greet name="Shaik" />
-      <Welcome name="Shagufta" alias="Shaggu" />
 
+      <Welcome name="Shagufta" alias="Shaggu" />
     </div>
   );
 }
-
 export default App;
-	
