25ffca3711392cf063ce52fb9a10e56bfa7c2a6f
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 16:45:11 +0530

    event handling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
 import ClassClick from './components/ClassClick';
 import FunctionClick from './components/FunctionClick';
-import EventBind from './components/EventBind';
 
 
 function App() {
   return (
     <div className="App">
-      <EventBind />
-      </div>
+      <Message />
+
+      <ClassClick />
+      <FunctionClick />
+
+      <Counter />
+
+      <Greet name="Shaggu" college="IIT">
+        <p> this is children property</p>
+      </Greet>
+      <Greet name="Shayi" college="Oxford" >
+        <button>click me</button>
+      </Greet >
+      <Greet name="Shaik" />
+      <Welcome name="Shagufta" alias="Shaggu" />
+    </div>
   );
 }
 export default App;
