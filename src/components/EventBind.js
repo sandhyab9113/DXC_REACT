a101d84bd84d6b9ef7c4e5c94da4e44b5e77e019
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 16:53:14 +0530

    binding in constructor

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-
 export class EventBind extends Component {
     constructor(props) {
         super(props)
-
         this.state = {
             message: 'Hello'
         }
+        this.clickHandler = this.clickHandler.bind(this)
     }
 
     clickHandler() {
@@ -15,15 +14,14 @@ export class EventBind extends Component {
             message: 'good bye'
         })
     }
-
-
     render() {
         //using bind im tying clickhandler to this keyword  -- binding in render method
         return (
             <div>
                 <div>{this.state.message}</div>
                 {/*<button onClick={this.clickHandler.bind(this)}>click</button>*/}
-                <button onClick= {() => this.clickHandler()}>click</button>
+                {/*<button onClick= {() => this.clickHandler()}>click</button>*/}
+                <button onClick={this.clickHandler}>click</button>
             </div>
         )
     }
