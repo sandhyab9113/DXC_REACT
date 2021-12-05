74e9daaea79f633277a6c7cda86c7ba77e448299
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 16:34:53 +0530

    destructing props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,17 +1,18 @@
-import react from 'react'
-import App from '../App'
-
-/* function Greet(){
-   return <h1>welcome sandy</h1>
-}
- */
-const Greet = (props) => {
-    console.log(props);
+import React from 'react'
+/*function Greet() {
+    return <h1>welcome Shagufta</h1>
+}*/
+//const Greet = () => <h1>hello dxc</h1>
+//const Greet = (props) => {
+const Greet = props => {
+    const { name, college } = props
+    // console.log(props);
+    //props.name = 'dxc'
     return (
-       <div>
-          <h1> hello  {props.name} you are from {props.college}</h1>
-          {props.children}
-       </div>
+        <div>
+            <h1> hello  {name} you are from {college}</h1>
+        </div>
     )
- }
- export default Greet
+
+}
+export default Greet
